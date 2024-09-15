f23873604b16edfc1391cd291ae08495964dfcd9
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-15 13:22:16 +0200

    update

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const home = () => {
   return (
     <View>
-      <Text style={{fontSize:40, fontFamily:'outfit-bold'} }>Home</Text>
+      <Text style={{ fontSize: 40, fontFamily: "outfit-bold" }}>Home</Text>
     </View>
-  )
-}
+  );
+};
 
-export default home
+export default home;
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,19 +1,28 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import {ClerkProvider} from 
+import { ClerkProvider, ClerkLoaded, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { Text } from "react-native";
 
 export default function RootLayout() {
-
   useFonts({
-    'outfit': require('./../assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium': require('./../assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf')
-  })
+    'outfit': require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    'outfit-bold"': require("./../assets/fonts/Outfit-Bold.ttf"),
+  });
 
   return (
-    <ClerkProvider></ClerkProvider>
-    <Stack screenOptions={{headerShown:false}}>
-      <Stack.Screen name="(tabs)"/>
-    </Stack>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <SignedIn>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+
+      <SignedOut>
+        <Text>SignedOut</Text></SignedOut>
+      
+    </ClerkProvider>
   );
 }
